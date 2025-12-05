4039403b5f0e43844444cbf1ae2af1a81d3f13d2
AhmedMHCodeLab <ahmedmh03.work@gmail.com>
2025-12-05 17:40:07 +0300

    Uncomment WAF CloudWatch logging configuration

--- infra/modules/waf/main.tf
+++ infra/modules/waf/main.tf
@@ -134,27 +134,27 @@ resource "aws_wafv2_web_acl_association" "alb" {
   web_acl_arn  = aws_wafv2_web_acl.main.arn
 }
 
-# CloudWatch Log Group for WAF (optional but useful)
-# resource "aws_cloudwatch_log_group" "waf" {
-#   name              = "/aws/wafv2/${var.project_name}"
-#   retention_in_days = 7
-#
-#   tags = {
-#     Name        = "${var.project_name}-waf-logs"
-#     Project     = var.project_name
-#     Environment = var.environment
-#     ManagedBy   = "Terraform"
-#   }
-# }
 
-# WAF Logging Configuration
-# resource "aws_wafv2_web_acl_logging_configuration" "main" {
-#   resource_arn            = aws_wafv2_web_acl.main.arn
-#   log_destination_configs = ["${aws_cloudwatch_log_group.waf.arn}:*"]
-#
-#   redacted_fields {
-#     single_header {
-#       name = "authorization"
-#     }
-#   }
-# }
+resource "aws_cloudwatch_log_group" "waf" {
+  name              = "/aws/wafv2/${var.project_name}"
+  retention_in_days = 7
+
+  tags = {
+    Name        = "${var.project_name}-waf-logs"
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
+
+
+resource "aws_wafv2_web_acl_logging_configuration" "main" {
+  resource_arn            = aws_wafv2_web_acl.main.arn
+  log_destination_configs = [aws_cloudwatch_log_group.waf.arn]
+
+  redacted_fields {
+    single_header {
+      name = "authorization"
+    }
+  }
+}
